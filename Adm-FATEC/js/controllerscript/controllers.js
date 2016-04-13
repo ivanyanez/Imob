0c0d7e6bbd1f04160bbd47e4cae18144b468ee80
iury <satriani-16@hotmail.com>
2016-04-12 23:17:14 -0300

    tabs

--- Adm-FATEC/js/controllerscript/controllers.js
+++ Adm-FATEC/js/controllerscript/controllers.js
@@ -14,27 +14,11 @@ app.controller("sidebarController", function($scope){
 	};
 });
 
-app.controller("naviterno_cadastroimovel"),function($scope){
-	
-	 $scope.tab_map = {
-  	dados: true,
-    valores: false,
-    tab3: false,
-  }
-  
-  $scope.activate_tab = function(tab){
-    for (var key in $scope.tab_map){
-    		if (key === tab){
-     				$scope.tab_map[key] = true;   
-        }
-        else{
-        	$scope.tab_map[key] = false;
-        }
-    }
-  };
+app.controller("naviterno_cadastroimovel", function($scope){
+
+	$scope.selected_item = 'dados';
+	$scope.setItem = function(item){
+		$scope.selected_item = item;
+	};
 });
-	
-	
-	
-}
 
